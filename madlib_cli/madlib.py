817bf86a6ecfc8909fb9bf974e6e355c0018a443
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-07-27 23:34:44 +0300

    adding lab3

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,64 @@
+
+import re
+from _pytest._code.code import TracebackEntry
+# import parse
+
+
+def read_template(filepath: str) -> str:
+    try:
+        with open(filepath, 'r') as file_1:
+            file_text = file_1.read()
+            return file_text.strip()
+    except:
+        raise
+
+
+'It was a {Adjective} and {Adjective} {Noun}'
+
+
+def parse_template(template: str) -> str:
+    pattern = r'{(.*?)}'  # match everything in a non-greedy way and capture it
+    striped_template = re.sub(pattern, '{}', template)
+    striped_parts = tuple(re.findall(pattern, template))
+
+    return striped_template, striped_parts
+
+
+def merge(bare_template: str, language_parts: list) -> str:
+    return bare_template.format(*language_parts)
+
+
+def writing_file(merged_template: str, filepath: str):
+    with open(filepath, 'a') as new_file:
+        # write the content to the file, using the write() method
+        new_file.write(merged_template)
+# print(merge("It was a {} and {} {}.", ("dark", "stormy", "night")))
+
+
+if __name__ == "_main_":
+
+    print(' **** Welcome to Madlib Game****\n***You need to enter a list of guessing words to play this game***\n ***and your guessed words should be an answer to the next questions and your answers should be separated by commas***\n')
+
+    def_template = read_template('assets/template.txt')
+
+    stripped_template, guessing_question = parse_template(def_template)
+
+    def collecting_input(input_words):
+
+        # input_words = ['Adjective', 'A First Name', 'Past Tense Verb', 'Plural Noun ?',
+        # 'Large Animal', 'A Girl\'s Name', 'Number 1-50', 'First Name\'s']
+        for word in input_words:
+            answers = []
+            user_input = input(f'***{word}***\n <')
+            answers.append(user_input)
+        return answers
+
+    user_answers = collecting_input(guessing_question)
+
+    final_template = merge(stripped_template, user_answers)
+    writing_file(final_template, 'assets/final_template.txt')
+    #print(merge(def_template, user_answers))
+
+# parse_template(def_template)
+# print(re.findall(r'?<=\{).+?(?=\}',
+# 'It was a {Adjective} and {Adjective} {Noun}.'))
