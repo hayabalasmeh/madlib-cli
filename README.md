74aed73ad0ff636a2b7b951c62597db9493ad4d6
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-07-29 17:01:18 +0300

    updating the file

---
+++ README.md
@@ -0,0 +1,11 @@
+## Author: Haya Balasmeh
+## Project name : madlib game
+## Project descreption: 
+
+## welcomeing message to the user, explaining the Madlib game.
+## Then Read a template Madlib file.
+## Then Prompt the user to submit a series of words to fit each of the required components of the Madlib template.
+## With the collected user inputs, populate the template such that each provided input is placed into the correct position within the template.
+## After the resulting Madlib has been completed, it will provide the completed response back to the user in the command line.
+## PR links:
+## 
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,6 @@
 
 import re
+from typing import Tuple
 from _pytest._code.code import TracebackEntry
 # import parse
 
@@ -13,10 +14,10 @@ def read_template(filepath: str) -> str:
         raise
 
 
-'It was a {Adjective} and {Adjective} {Noun}'
+# 'It was a {Adjective} and {Adjective} {Noun}'
 
 
-def parse_template(template: str) -> str:
+def parse_template(template: str) -> list:
     pattern = r'{(.*?)}'  # match everything in a non-greedy way and capture it
     striped_template = re.sub(pattern, '{}', template)
     striped_parts = tuple(re.findall(pattern, template))
@@ -24,8 +25,9 @@ def parse_template(template: str) -> str:
     return striped_template, striped_parts
 
 
-def merge(bare_template: str, language_parts: list) -> str:
-    return bare_template.format(*language_parts)
+def merge(bare_template: str, language_parts: tuple) -> str:
+    result = bare_template.format(*language_parts)
+    return result
 
 
 def writing_file(merged_template: str, filepath: str):
@@ -35,7 +37,7 @@ def writing_file(merged_template: str, filepath: str):
 # print(merge("It was a {} and {} {}.", ("dark", "stormy", "night")))
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
 
     print(' **** Welcome to Madlib Game****\n***You need to enter a list of guessing words to play this game***\n ***and your guessed words should be an answer to the next questions and your answers should be separated by commas***\n')
 
@@ -47,17 +49,21 @@ if __name__ == "_main_":
 
         # input_words = ['Adjective', 'A First Name', 'Past Tense Verb', 'Plural Noun ?',
         # 'Large Animal', 'A Girl\'s Name', 'Number 1-50', 'First Name\'s']
+        answers = []
         for word in input_words:
-            answers = []
+
             user_input = input(f'***{word}***\n <')
             answers.append(user_input)
+
         return answers
 
     user_answers = collecting_input(guessing_question)
-
+    print(user_answers)
+    print(stripped_template)
     final_template = merge(stripped_template, user_answers)
     writing_file(final_template, 'assets/final_template.txt')
-    #print(merge(def_template, user_answers))
+    print(final_template)
+
 
 # parse_template(def_template)
 # print(re.findall(r'?<=\{).+?(?=\}',
